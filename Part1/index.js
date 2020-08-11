7cef52a0d700bdb16f2b2260474d217f3ef5103d
Kara LeSueur <kara.lesueur@gmail.com>
2020-08-11 13:52:03 -0700

    clickedElement-ifelse-complete

--- Part1/index.js
+++ Part1/index.js
@@ -1,10 +1,13 @@
-function play() {
+function play( clickedId ) {
     const playerSpan = document.getElementById('player') 
+    const clickedElement = document.getElementById('clickedId')
     
         if (`playerSpan.innerText` === `X`) {
             `playerSpan.innerText` = `O`;
+            `clickedElement.innerText` = `X`;
         } else {
             `playerSpan.innerText` = `X`;
+            `clickedElement.innerText` = `O`;
         }
 }
 
