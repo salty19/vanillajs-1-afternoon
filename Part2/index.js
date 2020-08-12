9270a774e228d0cd02e0720f35a6c4b41e9972ba
Kara LeSueur <kara.lesueur@gmail.com>
2020-08-11 21:55:36 -0700

    almost-done-have-questions

--- Part2/index.js
+++ Part2/index.js
@@ -2,13 +2,19 @@ const idInput = document.getElementById(idInput);
 const colorInput = document.getElementById(colorInput);
 
 function setCard () {
-
+    const card = document.getElementById()
 }
+console.log(card)
+
+
 /*
 BELOW:
 1. setCard with no parameter
-2. 
+2. var called 'card'
+    -with getElementById taking in idInput.value
+*** NEED TO ASK ABOUT FUNCTION & WHAT IT'S DOING ***
+TAKING IN ARGUMENT WITH getElementById?
 // function setCard() {
 //     const card = document.getElementById(idInput.value);
 //     console.log(card);
-//   }
+//   
