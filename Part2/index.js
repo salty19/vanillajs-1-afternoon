02fd854cd4576303bfc032d25faa0efb009c46d2
Kara LeSueur <kara.lesueur@gmail.com>
2020-08-11 21:49:50 -0700

    trying-to-figure-out-function

--- Part2/index.js
+++ Part2/index.js
@@ -1,4 +1,14 @@
 const idInput = document.getElementById(idInput);
 const colorInput = document.getElementById(colorInput);
 
-function setCard
+function setCard () {
+
+}
+/*
+BELOW:
+1. setCard with no parameter
+2. 
+// function setCard() {
+//     const card = document.getElementById(idInput.value);
+//     console.log(card);
+//   }
